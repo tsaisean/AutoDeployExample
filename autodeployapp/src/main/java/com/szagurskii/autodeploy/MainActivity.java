90a678b623074472c2aff72bc821e81e953cf6c5
Sean Tsai <sean@Seande-MacBook-Pro.local>
2017-03-08 18:00:08 +0800

    test

--- autodeployapp/src/main/java/com/szagurskii/autodeploy/MainActivity.java
+++ autodeployapp/src/main/java/com/szagurskii/autodeploy/MainActivity.java
@@ -7,6 +7,6 @@ public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main2);
+    setContentView(R.layout.activity_main);
   }
 }
